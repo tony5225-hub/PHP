176db2cf11232c54964508d6b86be5b5ba5cabc0
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-06-04 06:59:39 -0300

    aula 4

--- aula4.php
+++ aula4.php
@@ -1,23 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
-    <title>Primeiro exemplo PHP</title>
-    <style>
-        h2 {
-            color: #00a5fe;
-            text-shadow: 1px 1px 1px black;
-        }
-    </style>
-
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
-
 <body>
-    <h1>Testando PHP</h1>
     <?php
-    echo "<h2>Olá,<br> Mundo!!!!</h2> ";
+    $n = 4;
+    $no = "Toninho ";
+    echo $no ;
     ?>
 </body>
-
 </html>
---
+++ ex001.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Primeiro exemplo PHP</title>
+    <style>
+        h2 {
+            color: #00a5fe;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style>
+
+</head>
+
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+    echo "<h2>Olá,<br> Mundo!!!!</h2> ";
+    ?>
+</body>
+
+</html>
