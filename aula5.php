f9304ddc08ae278fd0a6f7c5e4fe9f002a800449
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-06-11 07:23:53 -0300

    Update aula5.php
    atual

--- aula5.php
+++ aula5.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Operador Aritmetico</title>
+  <title>Somador</title>
   
 </head>
 
@@ -13,7 +13,7 @@
 
   <?php
   $n1 = 3;
-  $n2 = 2;
+  $n2 = 3;
   $s = $n1 + $n2;
   echo "A soma entre $n1 e $n2 é igual a $s ";
 
