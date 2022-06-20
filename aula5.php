9a07e21c6ae96fef39b6963810694e1a3562dd1b
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-06-20 05:19:58 -0300

    Update aula5.php
    atual

--- aula5.php
+++ aula5.php
@@ -5,18 +5,16 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Operador Aritmético</title>
+  <title>Operador Aritmetico</title>
   
 </head>
-
 <body>
 
   <?php
-  $n1 = 3;
-  $n2 = 2;
+    $n1 = 3;
+    $n2 = 2;
   
-    echo "A soma vale ". ($n1+$n2);
-
+    echo "A soma vale " ($n1+$n2);
   ?>
 
 
