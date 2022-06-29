d4b793f37f1e633905e8dc2e75f365a23785cc5e
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-06-29 04:53:54 -0300

    criei
    atualizei

---
+++ aula5fa.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>funcoes aritmeticas</title>
+</head>
+<body>
+    <?php
+    $v1 = $_GET ["x"];
+    $v2 = $_GET ["y"];
+    echo "<h2>Valores recebidos: $v1 e $v2</h2";
+    ?>
+</body>
+</html>
