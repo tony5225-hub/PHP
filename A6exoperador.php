c9c126318359f80c6145a7d0cf3b76f46a52c5bd
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-07-05 17:01:15 -0300

    Create A6exoperador.php
    atual

---
+++ A6exoperador.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercico de operadores</title>
+</head>
+<body>
+    <?php // este exercício mostra preço vai aparecer na url e jogar numa váriavel que vai chamar de preço.
+    $preco = $_GET ["p"];
+    echo "o preço do produto é R$ $preco ";
+    ?>
+</body>
+</html>
