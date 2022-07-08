c8dc1bb7c50efd6940071e970709705d319b7bcd
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-07-08 08:48:58 -0300

    Create A6ex02incremento.php
    atual

---
+++ A6ex02incremento.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercico de operadores</title>
+</head>
+<body>
+    <?php 
+    $atual = $_GET [aa];
+    echo "O Ano atual é $atual";
+    
+    ?>
+</body>
+</html>
