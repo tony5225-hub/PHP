283891214e5d1f69cbaff0e4f75b2feb998e4279
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-07-12 07:46:05 -0300

    atualizei

--- A6ex02incremento.php
+++ A6ex02incremento.php
@@ -8,8 +8,8 @@
 </head>
 <body>
     <?php 
-    $atual = $_GET [aa];
-    echo "O Ano atual é $atual";
+    $atual = $_GET ["aa"];
+    echo "O Ano atual é $atual e ano anterior é " . --$atual; /*aqui esse --$atual é o pré incremento pois ele vai utilizar a variável o ano atual e depois a diminuição do ano atual e qdo representado no navegador: localhost/olamundo/A6ex02incremento.php?aa=2022 vai mostrar assim no comando de exibição na tela : O Ano atual é 2022 e ano anterior é 2021 */ 
     
     ?>
 </body>
