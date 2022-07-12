0482e02612e3e12c4cc85ca1ca690a6fd9f77ed1
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-07-12 08:21:16 -0300

    Update A6ex03vareferenciada.php
    atual

--- A6ex03vareferenciada.php
+++ A6ex03vareferenciada.php
@@ -8,8 +8,14 @@
 </head>
 <body>
     <?php 
-    echo
-    
+        $a = 3;
+        $b = $a;
+        $b += 5;
+
+        echo "A variavel A vale $a";
+        echo "<br/> A variavel B vale $b ";
+
+                   
     ?>
    
 </body>
