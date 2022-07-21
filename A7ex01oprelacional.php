781c1ab2cc693804c98dceb2a07143487677c7eb
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-07-21 10:05:07 -0300

    Update A7ex01oprelacional.php
    atualizei

--- A7ex01oprelacional.php
+++ A7ex01oprelacional.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Operador Relacional</title>
+    <title>Operador Relacional unário</title>
 </head>
 <body>
     <?php 
@@ -14,8 +14,10 @@
        
    echo "os valores passados foram $n1 e $n2 <br/>";
    $r = ($tipo == "s") ? $n1+$n2 : $n1*$n2; // aqui interrogação "?" le-se como "SE" e dois pontos ":" como "ENTÃO".
-   echo "o resultado será $r";
-   
+   echo "o resultado será $r"; /* OBS: OPERADOR UNÁRIO É PARA FAZER COMPARAÇÕES BÁSICAS EM DOIS VALORES AQUI NESTE LINK DO NAVEGADOR SE COLOCAR o S ELE VAI SOMAR QUALQUER OUTRA LETRA FORA O S ELE VAI MULTPLICAR  veja NO FINAL DO LINK : "?a=5&b=3&op=m"  http://localhost/olamundo/A7ex01oprelacional.php?a=5&b=3&op=m
+   */
+
+      
     ?>
    
 </body>
