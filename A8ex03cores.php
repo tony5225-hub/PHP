2201475a4677987460eb881979afe40f4c01ec73
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-08-15 04:32:09 -0300

    atual

---
+++ A8ex03cores.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex03 cores</title>
+</head>
+<body>
+<div>
+<?php 
+      $txt = isset ($_GET["t"])?$_GET["t"]:"Texto Generico";
+      $tam = isset ($_GET["tam"])?$_GET["tam"]:"12pt";
+      $cor = isset ($_GET["cor"])?$_GET["cor"]:"000000";
+      ?>
+</div>
+
+</body>
+</html>
