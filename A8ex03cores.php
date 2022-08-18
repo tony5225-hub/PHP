2e5ef8263cfd7543cb07b038711a101b6d5516a2
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-08-18 04:42:44 -0300

    Update A8ex03cores.php
    atual

--- A8ex03cores.php
+++ A8ex03cores.php
@@ -12,6 +12,8 @@
       $txt = isset ($_GET["t"])?$_GET["t"]:"Texto Generico";
       $tam = isset ($_GET["tam"])?$_GET["tam"]:"12pt";
       $cor = isset ($_GET["cor"])?$_GET["cor"]:"000000";
+
+      echo "<span class=></span>";
       ?>
 </div>
 
