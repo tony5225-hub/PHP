aaef44c294d3f3990d7f0ac4a68dc1ec0b094d58
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-08-22 04:21:48 -0300

    Update A7ex01oprelacional.php
    atual

--- A7ex01oprelacional.php
+++ A7ex01oprelacional.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Operador Relacional unário</title>
+    <title>Operador Relacional ternário</title>
 </head>
 <body>
     <?php 
