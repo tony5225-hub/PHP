230d2f860c99e4c5169bd70924b9815423cdfe10
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-09-24 13:06:06 -0300

    atual

---
+++ exercicios/A10ex02.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso em PHP </title>
+</head>
+<body>
+    <div>
+        <?php
+        $d = isset ($_GET ["ds"])?$_GET["ds"]:0;
+       ?> 
+       <br/><a href="javascript:history.go(-1)"class="botao">voltar</a>
+    </div>
+</body>
+</html>
