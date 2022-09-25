ee0771f3a7db5574c9a6a12438691d0bb64b5c89
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-09-25 11:44:24 -0300

    Update A10ex02.php
    ataul

--- exercicios/A10ex02.php
+++ exercicios/A10ex02.php
@@ -10,6 +10,21 @@
     <div>
         <?php
         $d = isset ($_GET ["ds"])?$_GET["ds"]:0;
+        switch ($d){ 
+            case 2:
+            case 3:
+            case 4:
+            case 5: 
+            case 6:
+                echo  "levanta e vai estudar!:)";
+                break;
+            case 7: 
+            case 8:  
+                echo "Estudar o que alegria hahauahahauah!! "; 
+                break;
+            default:  
+            echo "Dia da semana inválido  ";                  
+        }
        ?> 
        <br/><a href="javascript:history.go(-1)"class="botao">voltar</a>
     </div>
