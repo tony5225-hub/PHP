2e06fbe3b58a88c4de334728de22f9638436e167
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2022-10-10 03:48:02 -0300

    Create A11ex02.php
    atual

---
+++ A11ex02.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP repetição</title>
+</head>
+<body>
+    <div>
+    <form method="get" action="02-parte2.php">
+    <?php
+         $n = 1;
+         while ($c <=5) {
+            echo "Valor $n: <input type ='number' name='v$c' max= '100' min= '0'/><br/>";
+            
+            $n++;
+         }
+        ?>
+             
+            <input type = "submit" value = "Enviar" class="botao"/> 
+        </form>
+        
+    </div>
+</body>
+</html>
